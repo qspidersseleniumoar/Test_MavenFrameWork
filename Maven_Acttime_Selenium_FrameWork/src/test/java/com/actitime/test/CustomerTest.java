6ef098f036ca508d9d2417e34552c499e48e97b8
deepak <seleniumoar1234@gmail.com>
2019-05-10 08:16:18 +0530

    push the frameWork to git

---
+++ Maven_Acttime_Selenium_FrameWork/src/test/java/com/actitime/test/CustomerTest.java
@@ -0,0 +1,29 @@
+package com.actitime.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test
+	public void createCustomerTest() {
+		System.out.println("execute createCustomerTest");
+	}
+	
+	@Test
+	public void modifyCustomerTest() {
+		System.out.println("execute modifyCustomerTest");
+	}
+	
+	@Test
+	public void deleteCustomerTest() {
+		System.out.println("execute deleteCustomerTest");
+	}
+	
+	@Test
+	public void searchCustomerTest() {
+		System.out.println("execute searchCustomerTest");
+	}
+
+}
